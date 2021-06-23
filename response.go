6aae71d754979673c6d75b59d296b064196e0177
koalabearguo <koalabearguo@gmail.com>
2021-06-23 02:49:20 -0400

    clean

--- response.go
+++ response.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"io"
-	//"log"
 	"net/http"
 )
 
@@ -15,12 +14,9 @@ type response struct {
 
 func (res *response) parse_response() {
 	res.body_buf = bytes.NewBuffer(nil)
-	//log.Printf("%s", res.res.Header)
+	//
 	encrypt := &encrypt{cfg: res.cfg}
-	//test_body_buf := bytes.NewBuffer(nil)
-	//io.Copy(test_body_buf, res.res.Body)
-	//encrypt.content_decrypt(test_body_buf, res.res.Body)
-	//log.Printf("%s",test_body_buf)
+	//
 	if len(res.res.Header["Content-Type"]) != 0 && res.res.Header["Content-Type"][0] == "image/gif" && res.res.StatusCode == http.StatusOK {
 		encrypt.content_decrypt(res.body_buf, res.res.Body)
 	} else {
