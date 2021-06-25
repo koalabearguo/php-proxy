0a455fea978d02f3165876d8aae21ec0f27f1b28
koalabearguo <koalabearguo@gmail.com>
2021-06-24 23:31:04 -0400

    when none proxy mode,HTTP/1.0 close

--- proxy.go
+++ proxy.go
@@ -97,7 +97,7 @@ func (prx *proxy) handleClientRequest(client net.Conn) {
 		}
 		req_op.https_req = Req
 	} else if !Req.URL.IsAbs() {
-		_, err = client.Write([]byte("HTTP/1.1 200 OK\r\n\r\nThis is php-proxy cilent."))
+		_, err = client.Write([]byte("HTTP/1.0 200 OK\r\n\r\nThis is php-proxy cilent."))
 		client.Close()
 		return
 	}
